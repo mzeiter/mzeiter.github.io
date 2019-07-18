afc5ad45b1bb162acb1abb07a24bc0dd2753c340
mgz33 <mgz33@acs.org>
2019-07-18 11:52:45 -0400

    Added new page for API fun stuff

--- scripts.js
+++ scripts.js
@@ -42,13 +42,13 @@ request.send() */
 
 const app = document.getElementById('root')
 
-const logo = document.createElement('img')
-logo.src = 'logo.png'
+/* const logo = document.createElement('img')
+logo.src = 'logo.png' */
 
 const container = document.createElement('div')
 container.setAttribute('class', 'container')
 
-app.appendChild(logo)
+/* app.appendChild(logo) */
 app.appendChild(container)
 
 var request = new XMLHttpRequest()
