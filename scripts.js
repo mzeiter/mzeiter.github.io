fb1e14069666d55f92651f8d3ce5193741f79b2f
mgz33 <mgz33@acs.org>
2019-07-18 12:31:27 -0400

    Bitcoin and Litecoin in one 'get' req

--- scripts.js
+++ scripts.js
@@ -42,39 +42,44 @@ request.send() */
 
 const app = document.getElementById('root')
 
-/* const logo = document.createElement('img')
-logo.src = 'logo.png' */
-
 const container = document.createElement('div')
 container.setAttribute('class', 'container')
 
-/* app.appendChild(logo) */
 app.appendChild(container)
 
 var request = new XMLHttpRequest()
-request.open('GET', 'https://api.coinranking.com/v1/public/coin/1', true)
+request.open('GET', 'https://api.coinranking.com/v1/public/coins?ids=1,7', true)
 request.onload = function() {
   // Begin accessing JSON data here
   var json = JSON.parse(this.response)
+  var coins = json.data.coins
+  console.log(coins)
+  
   if (request.status >= 200 && request.status < 400) {
-	  const card = document.createElement('div')
-	  card.setAttribute('class', 'card')
+	  coins.forEach(coin => {
+		  const card = document.createElement('div')
+		  card.setAttribute('class', 'card')
 
-	  const h1 = document.createElement('h1')
-	  h1.textContent = json.data.coin.name
+		  const h1 = document.createElement('h1')
+		  h1.textContent = coin.name
+		  
+		  const logo = document.createElement('img')
+		  logo.src = coin.iconUrl
 
-	  const h2 = document.createElement('h2')
-	  json.data.coin.price = json.data.coin.price.substring(0, 8)
-	  h2.textContent = `${json.data.coin.price} USD`
+		  const h2 = document.createElement('h2')
+		  coin.price = coin.price.substring(0, 8)
+		  h2.textContent = `${coin.price} USD`
 
-	  container.appendChild(card)
-	  card.appendChild(h1)
-	  card.appendChild(h2)
+		  container.appendChild(card)
+		  card.appendChild(h1)
+		  card.appendChild(logo)
+		  card.appendChild(h2)
+	  })
   } else {
     const errorMessage = document.createElement('Error')
     errorMessage.textContent = `API request is not working!`
     app.appendChild(errorMessage)
   }
 }
-
 request.send()
+
