51be0ab5b99dfc25263748e19e98539c5d62f992
mzeiter <zeiter.14@osu.edu>
2019-08-14 13:16:19 -0400

    Updates to front page

--- scripts.js
+++ scripts.js
@@ -1,84 +1,71 @@
-/* const app = document.getElementById('root')
-
-const logo = document.createElement('img')
-logo.src = 'logo.png'
-
-const container = document.createElement('div')
-container.setAttribute('class', 'container')
-
-app.appendChild(logo)
-app.appendChild(container)
-
-var request = new XMLHttpRequest()
-request.open('GET', 'https://ghibliapi.herokuapp.com/films?limit=6', true)
-request.onload = function() {
-  // Begin accessing JSON data here
-  var data = JSON.parse(this.response)
-  if (request.status >= 200 && request.status < 400) {
-    data.forEach(movie => {
-      const card = document.createElement('div')
-      card.setAttribute('class', 'card')
-
-      const h1 = document.createElement('h1')
-      h1.textContent = movie.title
-
-      const p = document.createElement('p')
-      movie.description = movie.description.substring(0, 300)
-      p.textContent = `${movie.description}...`
-
-      container.appendChild(card)
-      card.appendChild(h1)
-      card.appendChild(p)
-    })
-  } else {
-    const errorMessage = document.createElement('marquee')
-    errorMessage.textContent = `Gah, it's not working!`
-    app.appendChild(errorMessage)
-  }
-}
-
-request.send() */
-
-
-const app = document.getElementById('root')
-const container = document.createElement('div')
-container.setAttribute('class', 'container')
-
-app.appendChild(container)
-
-var request = new XMLHttpRequest()
-request.open('GET', 'https://api.coinranking.com/v1/public/coins?symbols=btc,doge,ltc,eth,bch,xrp,miota,usdt,eos', true)
-request.onload = function() {
-  // Begin accessing JSON data here
-  var json = JSON.parse(this.response)
-  var coins = json.data.coins
-  console.log(coins)
+// $(document).ready(function() {
   
-  if (request.status >= 200 && request.status < 400) {
-	  coins.forEach(coin => {
-		  const card = document.createElement('div')
-		  card.setAttribute('class', 'card')
+//   $(window).scroll(function () {
+//       //if you hard code, then use console
+//       //.log to determine when you want the 
+//       //nav bar to stick.  
+//       console.log($(window).scrollTop())
+//     if ($(window).scrollTop() > 280) {
+//       $('#main').addClass('navbar-fixed');
+//     }
+//     if ($(window).scrollTop() < 281) {
+//       $('#main').removeClass('navbar-fixed');
+//     }
+//   });
+// });
 
-		  const h1 = document.createElement('h1')
-		  h1.textContent = coin.name
+// var prevScrollpos = window.pageYOffset;
+// window.onscroll = function() {
+//   var currentScrollPos = window.pageYOffset;
+//   if (prevScrollpos > currentScrollPos) {
+//     document.getElementById("main").style.top = "0";
+//   } else {
+//     document.getElementById("main").style.top = "-50px";
+//   }
+//   prevScrollpos = currentScrollPos;
+// }
+
+
+
+// const app = document.getElementById('root')
+// const container = document.createElement('div')
+// container.setAttribute('class', 'container')
+
+// app.appendChild(container)
+
+// var request = new XMLHttpRequest()
+// request.open('GET', 'https://api.coinranking.com/v1/public/coins?symbols=btc,doge,ltc,eth,bch,xrp,miota,usdt,eos', true)
+// request.onload = function() {
+//   // Begin accessing JSON data here
+//   var json = JSON.parse(this.response)
+//   var coins = json.data.coins
+//   console.log(coins)
+  
+//   if (request.status >= 200 && request.status < 400) {
+// 	  coins.forEach(coin => {
+// 		  const card = document.createElement('div')
+// 		  card.setAttribute('class', 'card')
+
+// 		  const h1 = document.createElement('h1')
+// 		  h1.textContent = coin.name
 		  
-		  const logo = document.createElement('img')
-		  logo.src = coin.iconUrl
+// 		  const logo = document.createElement('img')
+// 		  logo.src = coin.iconUrl
 
-		  const h2 = document.createElement('h2')
-		  coin.price = coin.price.substring(0, 8)
-		  h2.textContent = `${coin.price} USD`
+// 		  const h2 = document.createElement('h2')
+// 		  coin.price = coin.price.substring(0, 8)
+// 		  h2.textContent = `${coin.price} USD`
 
-		  container.appendChild(card)
-		  card.appendChild(h1)
-		  card.appendChild(logo)
-		  card.appendChild(h2)
-	  })
-  } else {
-    const errorMessage = document.createElement('Error')
-    errorMessage.textContent = `API request is not working!`
-    app.appendChild(errorMessage)
-  }
-}
-request.send()
+// 		  container.appendChild(card)
+// 		  card.appendChild(h1)
+// 		  card.appendChild(logo)
+// 		  card.appendChild(h2)
+// 	  })
+//   } else {
+//     const errorMessage = document.createElement('Error')
+//     errorMessage.textContent = `API request is not working!`
+//     app.appendChild(errorMessage)
+//   }
+// }
+// request.send()
 
